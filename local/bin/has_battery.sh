a2b6a825e37b9ea0ca1a1e48d7201f3fcd09b365
Matteo Sambruna <29207130+senesc@users.noreply.github.com>
2023-12-13 18:20:21 +0100

    show tmux battery status only if device has a battery

---
+++ local/bin/has_battery.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Checks for presence of battery.
+# Returns 0 if any battery is available, 1 otherwise.
+
+for d in $(ls /sys/class/power_supply/)
+do
+	d=/sys/class/power_supply/$d/type
+	if grep -q 'Battery' $d; then
+		# echo "Battery found at $d"
+		exit 0
+	# else
+	# 	echo "$d not a battery"
+	fi
+done
+exit 1
